c18aa5a08cad755366e0984e0912525869cc5ce2
vidarnilsson <vidnil-3@student.ltu.se>
2025-11-11 22:45:37 +0100

    Update renderer.py

--- PhysicsSim/GUI/renderer.py
+++ PhysicsSim/GUI/renderer.py
@@ -6,12 +6,10 @@ class Renderer:
 
     def draw(self, snapshot, camera):
         # snapshot: immutable read-only view of world (positions, shapes)
-        for body in snapshot.bodies:
-            x, y = camera.world_to_screen(body.pos)
-            if body.shape_type == "circle":
-                pygame.draw.circle(self.surface, (40,40,40),
-                                   (int(x), int(y)), int(body.radius), 2)
-            elif body.shape_type == "aabb":
-                hw, hh = body.half_size
-                rect = pygame.Rect(int(x - hw), int(y - hh), int(2*hw), int(2*hh))
-                pygame.draw.rect(self.surface, (40,40,40), rect, 2)
+        x, y = 40, 50
+
+        pygame.draw.circle(self.surface, (40,40,40),(int(x), int(y)), int(20), 2)
+
+        hw, hh = 100, 500
+        rect = pygame.Rect(int(x - hw), int(y - hh), int(2*hw), int(2*hh))
+        pygame.draw.rect(self.surface, (40,40,40), rect, 2)
